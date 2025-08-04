7b3e4b51bf58bb86f6cc12b479d39e27e8f01914
Amal Ritessh A P <116954112+AmalRitessh@users.noreply.github.com>
2025-08-04 18:23:31 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -70,3 +70,7 @@ def run_endpoint(
     return {
         "answers": ans
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
